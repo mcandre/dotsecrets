41f94418a7b0050c32ec5775fb98ff495a24e655
Andrew Pennebaker <andrew.pennebaker@gmail.com>
2018-11-11 17:57:02 -0600

    update editorconfig

--- dotsecret.m
+++ dotsecret.m
@@ -3,14 +3,14 @@
 %% Default luminance = 205.
 %% Default tolerance = 5.
 function M = dotsecret(A, luminance, tolerance)
-  switch nargin
-    case 1
-      luminance = 205;
-      tolerance = 5;
-    case 2
-      tolerance = 5;
-  end
+    switch nargin
+        case 1
+        luminance = 205;
+        tolerance = 5;
+        case 2
+        tolerance = 5;
+    end
 
-  B = rgb2gray(A);
-  M = roicolor(B, luminance - tolerance, luminance + tolerance);
+    B = rgb2gray(A);
+    M = roicolor(B, luminance - tolerance, luminance + tolerance);
 end
